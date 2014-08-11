691bc9224816a2f1f923537699fb9d651285e475
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-11 19:18:00 +0200

    Fixed template with booktabs package and updated release.sh.

--- release.sh
+++ release.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-cp eclipse_basics.pdf ../../public/2014
-cp bpmn.pdf ../../public/2014
-cp emf.pdf ../../public/2014
-cp cassandra.pdf ../../public/2014
-cp yakindu.pdf ../../public/2014
-cp web_services.pdf ../../public/2014
-cp bonita_connector.pdf ../../public/2014
-cp code_generation.pdf ../../public/2014
-cp nosql.pdf ../../public/2014
-cp incquery.pdf ../../public/2014
-cp rest_service_docs.pdf ../../public/2014
-cd ../../public/2014
-svn add *.pdf
-svn commit -m "releasing notes"
+
+pushd .
+
+TARGET_DIR=../ftsrg.github.io/mdsd/2014/
+
+cp eclipse_basics.pdf $TARGET_DIR
+
+cp code_generation.pdf $TARGET_DIR
+cp emf.pdf $TARGET_DIR
+cp incquery.pdf $TARGET_DIR
+cp yakindu.pdf $TARGET_DIR
+
+cp bonita_connector.pdf $TARGET_DIR
+cp bpmn.pdf $TARGET_DIR
+cp cassandra.pdf $TARGET_DIR
+cp nosql.pdf $TARGET_DIR
+cp web_services.pdf $TARGET_DIR
+
+cd $TARGET_DIR
+git add *.pdf
+git commit -m "Releasing notes."
+git push
+
+popd
